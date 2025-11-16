b7e58466f9d778212546ceba8cd955a577490098
La Tahzan <133788550+Pangleran@users.noreply.github.com>
2025-11-16 11:50:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,13 @@
   <meta name="viewport" content="width-device-width, initial-scale=1.0">
   <title>Document</title> 
 </head>
+<body>
+  <?php include "layout/header.html" ?>
+  
+  <main>
+    <p>Welcome in website</p>
+  </main>
 
-** code
+  <?php include "layout/footer.html" ?>
+</body>
+</html>
